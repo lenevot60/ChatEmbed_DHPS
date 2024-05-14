b4433d8760159db35681b102697405c3ee29ac58
lenevot60 <156949783+lenevot60@users.noreply.github.com>
2024-05-14 10:42:24 +0000

    Botbubble - Borderradius ( 6px > 12px)

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -168,7 +168,7 @@ export const BotBubble = (props: Props) => {
           style={{
             'background-color': props.backgroundColor ?? defaultBackgroundColor,
             color: props.textColor ?? defaultTextColor,
-            'border-radius': '6px',
+            'border-radius': '12px',
             'font-size': props.fontSize ? `${props.fontSize}px` : `${defaultFontSize}`,
           }}
         />
